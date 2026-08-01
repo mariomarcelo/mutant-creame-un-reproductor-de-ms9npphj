e931aab949e0fdcbe7c8534ccefb325690a8e726
mariomarcelo <53236511+mariomarcelo@users.noreply.github.com>
2026-07-31 21:54:57 -0300

    MUTANT: create app/index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Layout from './_layout';
+
+const App = () => {
+  return (
+    <Layout />
+  );
+};
+
+export default App;
