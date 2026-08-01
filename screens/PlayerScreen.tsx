68a32315fbdc1489dd6b59bb616a9d10e8347737
mariomarcelo <53236511+mariomarcelo@users.noreply.github.com>
2026-07-31 21:54:59 -0300

    MUTANT: create screens/PlayerScreen.tsx

---
+++ screens/PlayerScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Player from '../../components/Player';
+
+const PlayerScreen = () => {
+  return (
+    <View>
+      <Player />
+    </View>
+  );
+};
+
+export default PlayerScreen;
